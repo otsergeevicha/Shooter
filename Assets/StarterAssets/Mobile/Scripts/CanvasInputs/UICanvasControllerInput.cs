ccc26b5b0dc971741bb7bc6063b277e2784fa253
Alexandr Ivanov <otsergeevicha@gmail.com>
2023-05-19 15:38:22 +0300

    Minor refactoring

---
+++ Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public class UICanvasControllerInput : MonoBehaviour
+    {
+
+        [Header("Output")]
+        public StarterAssetsInputs starterAssetsInputs;
+
+        public void VirtualMoveInput(Vector2 virtualMoveDirection)
+        {
+            starterAssetsInputs.MoveInput(virtualMoveDirection);
+        }
+
+        public void VirtualLookInput(Vector2 virtualLookDirection)
+        {
+            starterAssetsInputs.LookInput(virtualLookDirection);
+        }
+
+        public void VirtualJumpInput(bool virtualJumpState)
+        {
+            starterAssetsInputs.JumpInput(virtualJumpState);
+        }
+
+        public void VirtualSprintInput(bool virtualSprintState)
+        {
+            starterAssetsInputs.SprintInput(virtualSprintState);
+        }
+        
+        
+        public void VirtualAimInput(bool virtualSprintState)
+        {
+            starterAssetsInputs.AimInput(virtualSprintState);
+        }
+        
+        public void VirtualShootInput(bool virtualJumpState)
+        {
+            starterAssetsInputs.ShootInput(virtualJumpState);
+        }
+    }
+}
